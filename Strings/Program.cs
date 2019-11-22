348e66579e28a2e59b0fbe545600e10055036286
kierepkae <kierepkaedyta@gmail.com>
2019-11-22 16:40:51 +0100

    use Substring, Replace, Split, ToString methods

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -6,7 +6,23 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fullName = "Joe Doe ";
+            Console.WriteLine("Trim: {0}", fullName.Trim());
+            Console.WriteLine("ToUpper: {0}", fullName.Trim().ToUpper());
+            //var index = fullName.IndexOf(" ");
+            //var firstName = fullName.Substring(0, index);
+            //var lastName = fullName.Substring(index +1);
+            //Console.WriteLine("First Name: " + firstName);
+            //Console.WriteLine("Last Name: " + lastName);
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
+            Console.WriteLine("Names: {0}", fullName.Replace("Joe", "Joseph"));
+            var str = "22";
+            var age = Convert.ToInt32(str);
+            Console.WriteLine(age);
+            float price = 22.95f;
+            Console.WriteLine(price.ToString("C"));
         }
     }
 }
